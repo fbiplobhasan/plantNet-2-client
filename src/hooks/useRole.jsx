34620786be636842da23d30abff86060df4d6964
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-10-08 22:50:25 +0600

    add all-users data in admin manage users page

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -5,7 +5,7 @@ import useAxiosSecure from "./useAxiosSecure";
 const useRole = () => {
   const axiosSecure = useAxiosSecure();
   const { user, loading } = useAuth();
-  const { data: role, isLoading } = useQuery({
+  const { data: role = '', isLoading } = useQuery({
     queryKey: ["role", user?.email],
     enabled: !loading && !!user?.email,
     queryFn: async () => {
